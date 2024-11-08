294a8f3226c3364f618e90a7afef84493a6eed20
anubhav bisht <anubhavbisht98@gmail.com>
2024-11-08 06:36:20 +0530

    set up form and other components.Learnt about snaity and created a basic setup

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,4 +1,13 @@
-export default function Home() {
+import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartUpCard";
+
+export default async function Home({
+  searchParams
+}: {
+  searchParams: Promise<{ query?: string }>
+}) {
+  const queryParams = (await searchParams)?.query
+  const posts = []
   return <>
     <section className="pink_container">
       <h1 className="heading">
@@ -9,6 +18,21 @@ export default function Home() {
         Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
         Competitions.
       </p>
+      <SearchForm query={queryParams} />
+    </section>
+    <section className="section_container">
+      <p className="text-30-semibold">
+        {queryParams ? `Search results for "${queryParams}"` : "All Startups"}
+      </p>
+      <ul className="mt-7 card_grid">
+        {posts?.length > 0 ? (
+          posts.map((post: StartupTypeCard) => (
+            <StartupCard key={post?._id} post={post} />
+          ))
+        ) : (
+          <p className="no-results">No startups found</p>
+        )}
+      </ul>
     </section>
   </>
 }
